d9b5cc02d75e7804d388ae6d30e040c7d61e88b6
Pranav Krishnaa S <pranavkrishnaa.2003@gmail.com>
2024-10-10 12:11:17 +0530

    Add files via upload

---
+++ app/admin/subscription/page.jsx
@@ -0,0 +1,60 @@
+'use client'
+import SubsTableItem from '@/Components/Admin_components/SubsTableItem'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify';
+
+const page = () => {
+
+  const [emails,setEmails] = useState([]);
+
+  const fetchEmails = async () => {
+    const response = await axios.get('/api/email');
+    setEmails(response.data.emails);
+  }
+
+  const deleteEmail = async (id) => {
+    const response = await axios.delete('/api/email',{
+      params:{
+        id:id
+      }
+    })
+    if(response.data.success)
+    {
+      toast.success(response.data.msg);
+      fetchEmails();
+    }
+    else
+    {
+      toast.error('Subscription removal failed')
+    }
+  }
+
+  useEffect(() => {
+    fetchEmails();
+  },[]);
+
+  return (
+    <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
+        <h1>All subscriptions</h1>
+        <div className='relative max-w-[600px] h-[80vh] overflow-x-auto mt-4 border border-gray-400 scrollbar_hide'>
+          <table className='w-full text-sm text-gray-500'>
+            <thead className='text-xs text-left text-gray-700 uppercase bg-gray-50'>
+              <tr>
+                <th scope='col' className='px-6 py-3'>Email</th>
+                <th scope='col' className='px-6 py-3 hidden sm:block'>Date</th>
+                <th scope='col' className='px-6 py-3'>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {emails.map((item,index) => {
+                return <SubsTableItem key={index} email={item.email} date={item.date} id={item._id} deleteEmail={deleteEmail} />
+              })}
+            </tbody>
+          </table>
+        </div>
+    </div>
+  )
+}
+
+export default page
---
+++ app/api/blog/route.js
@@ -0,0 +1,64 @@
+import { ConnectDB } from "@/lib/config/db";
+import blogModel from "@/lib/models/blogModel";
+import { writeFile } from 'fs/promises';
+const { NextResponse } = require("next/server");
+const fs = require('fs');
+
+const LoadDB = async () => {
+    await ConnectDB();
+}
+
+LoadDB();
+
+export async function GET(request) {
+    const blogId = request.nextUrl.searchParams.get('id');
+    if(blogId)
+    {
+        const blog = await blogModel.findById(blogId);
+        return NextResponse.json(blog);
+    }
+    else
+    {
+        const blogs = await blogModel.find({});
+        return NextResponse.json({blogs});
+    }
+}
+
+export async function POST(request) {
+    const formData = await request.formData();
+    const timestamp = Date.now();
+    const image = formData.get('image');
+    const imageByteData = await image.arrayBuffer();
+    const buffer = Buffer.from(imageByteData);
+    const path = `./public/${timestamp}_${image.name}`;
+
+    await writeFile(path,buffer);
+
+    const imageURL = `/${timestamp}_${image.name}`;
+
+    const blogData = {
+        title:`${formData.get('title')}`,
+        description:`${formData.get('description')}`,
+        category:`${formData.get('category')}`,
+        author:`${formData.get('author')}`,
+        image: `${imageURL}`,
+        authorImg: `${formData.get('authorImg')}`
+    }
+
+    await blogModel.create(blogData);
+
+    return NextResponse.json({success:true,msg:'Blog Added'});
+}
+
+export async function DELETE(request) {
+
+    const blogId = request.nextUrl.searchParams.get('id');
+
+    const blog = await blogModel.findById(blogId);
+
+    fs.unlink(`./public/${blog.image}`,() => {});
+
+    await blogModel.findByIdAndDelete(blogId);
+
+    return NextResponse.json({msg:'Blog deleted successfully'});
+}
